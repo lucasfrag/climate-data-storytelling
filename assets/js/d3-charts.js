450bd4787b219ef126cda8ed0b2c97b6ad0e6ea4
Lucas Fraga <ti.lucasfraga@gmail.com>
2025-06-06 12:01:14 -0300

    Fix chart responsive

--- assets/js/d3-charts.js
+++ assets/js/d3-charts.js
@@ -1,17 +1,32 @@
 // Funções de animação
 function animarLinha() {
-    const svg = d3.select("#grafico-linha"),
-        margin = { top: 20, right: 30, bottom: 30, left: 50 },
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+    const svg = d3.select("#grafico-linha");
+    svg.selectAll("*").remove();
+   
+    const bounds = svg.node().getBoundingClientRect(),
+          margin = { top: 20, right: 30, bottom: 30, left: 50 },
+          width = bounds.width - margin.left - margin.right,
+          height = bounds.height - margin.top - margin.bottom,
+          g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const x = d3.scaleLinear().domain(d3.extent(dados, d => d.ano)).range([0, width]);
-    const y = d3.scaleLinear().domain([20, d3.max(dados, d => d.temp)]).nice().range([height, 0]);
-    const line = d3.line().x(d => x(d.ano)).y(d => y(d.temp));
+    const x = d3.scaleLinear()
+        .domain(d3.extent(dados, d => d.ano))
+        .range([0, width]);
 
-    g.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x).tickFormat(d3.format("d")));
-    g.append("g").call(d3.axisLeft(y));
+    const y = d3.scaleLinear()
+        .domain([20, d3.max(dados, d => d.temp)]).nice()
+        .range([height, 0]);
+
+    const line = d3.line()
+        .x(d => x(d.ano))
+        .y(d => y(d.temp));
+
+    g.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+    g.append("g")
+        .call(d3.axisLeft(y));
 
     const path = g.append("path")
         .datum(dados)
@@ -21,8 +36,10 @@ function animarLinha() {
         .attr("d", line);
 
     const totalLength = path.node().getTotalLength();
-    path.attr("stroke-dasharray", totalLength).attr("stroke-dashoffset", totalLength)
-        .transition().duration(2000).attr("stroke-dashoffset", 0);
+    path.attr("stroke-dasharray", totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition().duration(2000)
+        .attr("stroke-dashoffset", 0);
 
     g.selectAll("circle")
         .data(dados)
@@ -39,6 +56,7 @@ function animarLinha() {
         })
         .on("mouseout", () => tooltip.style("opacity", 0));
 }
+
 
 function animarArea() {
     const svg = d3.select("#grafico-area"),
